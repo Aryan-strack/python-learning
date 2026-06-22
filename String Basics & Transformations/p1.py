ba04e064274dcd7f0b7c1bee681370c3abb54e95
Aryan-strack <aryanahmad478@gmail.com>
2026-06-22 16:55:25 +0500

    p1

---
+++ String Basics & Transformations/p1.py
@@ -0,0 +1,26 @@
+# single and triple 
+
+name = "Aryan"
+print(name)
+
+message = '''hello Aryan,
+welcome to the world of programming.
+Have fun learning!
+'''
+
+print(message)
+
+# string Indexing
+
+word = "Programming"
+print(word[0])  # P
+print(word[3])  # g 
+print(word[-1])  # g
+print(word[-3])  # i
+
+# string slicing
+
+print(word[0:6])  # Program
+print(word[3:6])  # gra
+print(word[3:8])  # gram
+print(word[3:])   # gramming
